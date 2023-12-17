862fdc166efc13abde28f57be12629388f20ef82
Krystal <jian.yang@oebiotech.com>
2023-12-17 21:14:55 +0800

    feat(dragon): add component library Element Plus

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
 import pinia from '@/stores'
 
 const app = createApp(App)
 
 app.use(pinia)
 app.use(router)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.use(ElementPlus, { size: 'small' })
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,60 @@
 import { fileURLToPath, URL } from 'node:url'
+import * as path from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Inspect from 'vite-plugin-inspect'
+
+const srcPath = fileURLToPath(new URL('./src', import.meta.url))
+const dtsPath = path.resolve(srcPath, 'dts')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': srcPath
+    }
+  },
+
   plugins: [
     vue(),
     vueJsx(),
+
+    AutoImport({
+      // Auto import functions from Vue, e.g. ref, reactive, toRef...
+      imports: ['vue'],
+      resolvers: [
+        ElementPlusResolver(),
+        // Auto import icon components
+        IconsResolver({
+          prefix: 'Icon',
+        }),
+      ],
+      dts: path.resolve(dtsPath, 'auto-imports.d.ts')
+    }),
+
+    Components({
+      resolvers: [
+        // Auto register icon components
+        IconsResolver({
+          enabledCollections: ['ep'],
+        }),
+        // Auto register Element Plus components
+        ElementPlusResolver()
+      ],
+      dts: path.resolve(dtsPath, 'components.d.ts'),
+    }),
+
+    Icons({
+      autoInstall: true,
+    }),
+
+    Inspect(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
 })
