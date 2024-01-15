f4500e1c8313a1432c1a0721f7b38e69cfb199a2
joker <invictuslab001@gmail.com>
2024-01-16 00:34:00 +0800

    feat(dragon): add a new common environment variable configuration file .env

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,7 @@ app.use(router)
 
 app.component('svg-icon', SvgIcon)
 
+// Setting the CSS style of naive-ui has a higher priority than the reset style of tailwind.
 const meta = document.createElement('meta')
 meta.name = 'naive-ui-style'
 document.head.appendChild(meta)
