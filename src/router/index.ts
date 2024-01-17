fb7040f2fb9689b89def978c49f9d47749f79c37
joker <invictuslab001@gmail.com>
2024-01-18 00:49:32 +0800

    refactor(dragon): remove nprogress

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import NProgress from '@/config/nprogress'
 
 import staticRoutes from '@/router/static-routes'
 
@@ -11,18 +10,14 @@ const router = createRouter({
 
 // 路由拦截守卫
 router.beforeEach(async (to, from, next) => {
-  NProgress.start()
   next()
 })
 
 // 路由跳转结束
-router.afterEach(() => {
-  NProgress.done()
-})
+router.afterEach(() => {})
 
 // 路由跳转失败
 router.onError((error) => {
-  NProgress.done()
   console.log('路由跳转失败', error.message)
 })
 
